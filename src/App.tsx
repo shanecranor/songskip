af4eeac6dc0c3155a1a5f14049090fe306e6bc7b
shanecranor <shanecranor@gmail.com>
2024-12-13 21:52:15 -0600

    use observables

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import  { useState, ChangeEvent } from 'react';
+import  { ChangeEvent } from 'react';
+import {useObservable} from '@legendapp/state/react'
 import './App.css';
 import { SpotifyStreamingData } from './types';
+import { all, desc, op, table } from 'arquero';
 
 function App() {
-  const [file, setFile] = useState<File | null>(null);
-  const [fileContent, setFileContent] = useState<SpotifyStreamingData[] | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const file$ = useObservable<File | null>()
+  const fileContent$ = useObservable<SpotifyStreamingData[] | null>(null)
+  const error$ = useObservable<string | null>(null)
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
     if (selectedFile) {
-      setFile(selectedFile);
+      file$.set(selectedFile);
       readFileContent(selectedFile);
-      setError(null); // Clear any previous errors
+      error$.set(null); // Clear any previous errors
     } else {
-      setFile(null);
-      setFileContent(null);
-      setError('No file selected.');
+      file$.set(null);
+      fileContent$.set(null);
+      error$.set('No file selected.');
     }
   };
 
@@ -27,38 +29,38 @@ function App() {
       try {
         if (event.target?.result) {
           const jsonContent = JSON.parse(event.target.result as string); // Parse as JSON
-          setFileContent(jsonContent); // Update state with the parsed JSON object
+          fileContent$.set(jsonContent); // Update state with the parsed JSON object
         } else {
-          setError('File is empty or has no content.');
-          setFileContent(null);
+          error$.set('File is empty or has no content.');
+          fileContent$.set(null);
         }
       } catch (error) { // Catch JSON parsing errors
-        setError('Error parsing JSON: ' + (error as Error).message);
-        setFileContent(null);
+        error$.set('Error parsing JSON: ' + (error as Error).message);
+        fileContent$.set(null);
       }
     };
 
     reader.onerror = () => {
-      setError('Error reading file.');
-      setFileContent(null);
+      error$.set('Error reading file.');
+      fileContent$.set(null);
     };
 
 
     reader.readAsText(file); // Important: Read as text
   };
-
+  const file = file$.get()
   return (
     <>
       <div>
         <h1>songskip proof of concept</h1>
         <p>upload your spotify GDPR data</p>
         <input type="file" onChange={handleFileChange} /> 
-        {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error messages */}
+        {error$.get() && <p style={{ color: 'red' }}>{error$.get()}</p>} {/* Display error messages */}
         {file && <p>Selected File: {file.name}</p>} {/* Display selected file name */}
-        {fileContent && (
+        {fileContent$.get() && (
           <div>
             <h2>File Content:</h2>
-            <pre>{JSON.stringify(fileContent[0])}</pre> {/* Use <pre> for preserving formatting */}
+            <pre>{JSON.stringify(fileContent$[0].get())}</pre> {/* Use <pre> for preserving formatting */}
           </div>
         )}
       </div>
